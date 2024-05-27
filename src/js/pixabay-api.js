faa9b60c5eeabf24e4d5f8c14e413bad763674a9
Eva52975 <pavliuk.evelina@gmail.com>
2024-05-28 00:01:58 +0300

    homework 11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+export function findImage(inputValue) {
+  const BASE_URL = 'https://pixabay.com';
+  const END_POINT = '/api/';
+  const params = new URLSearchParams({
+    key: '44054875-23597af336816bebd19227040',
+    q: inputValue,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  const url = `${BASE_URL}${END_POINT}?${params}`;
+
+  const headers = {};
+
+  return fetch(url, { headers }).then(res => res.json());
+}
